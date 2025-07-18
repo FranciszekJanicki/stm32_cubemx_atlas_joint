9d645db6f04dfb862d28e216ecd80c837df7b1a7
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-18 12:03:43 +0200

    changes

--- components/atlas_core/atlas_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef ATLAS_CORE_ATLAS_CONFIG_H
-#define ATLAS_CORE_ATLAS_CONFIG_H
-
-#include "atlas_data.h"
-#include "atlas_joint_num.h"
-#include <stdint.h>
-
-#endif // ATLAS_CORE_ATLAS_CONFIG_H
