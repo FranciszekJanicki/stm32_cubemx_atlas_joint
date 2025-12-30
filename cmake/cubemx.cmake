0af8419af4fd73b9d90fae1b2183088dc1da9cdf
FranciszekJanicki <franciszek.janicki1@gmail.com>
2025-12-30 15:40:48 +0100

    changes

--- cmake/cubemx.cmake
+++ cmake/cubemx.cmake
@@ -9,3 +9,12 @@ if(EXISTS ${CUBEMX_CMAKE_FILE})
 else()
     message(WARNING "CubeMX CMake file not found: ${CUBEMX_CMAKE_FILE}")
 endif()
+
+function(setup_cubemx)
+    add_library(cubemx STATIC)
+    target_sources(cubemx PRIVATE ${MX_Application_Src})
+    target_link_libraries(cubemx PUBLIC ${MX_LINK_LIBS})
+    target_link_directories(cubemx PUBLIC ${MX_LINK_DIRS})
+endfunction(setup_cubemx)
+
+
