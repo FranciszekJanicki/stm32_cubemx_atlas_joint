69932b58db3991ec16080ba89f0b0e905e343d3c
FranciszekJanicki <franciszek.janicki1@gmail.com>
2026-01-01 13:07:17 +0100

    changes

--- cmake/components.cmake
+++
@@ -1,7 +0,0 @@
-include_guard(GLOBAL)
-
-include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
-
-function(setup_components)
-    add_subdirectories(${COMPONENTS_DIR})
-endfunction(setup_components)
--- cmake/main.cmake
+++
@@ -1,7 +0,0 @@
-include_guard(GLOBAL)
-
-include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
-
-function(setup_main)
-    add_subdirectory(${MAIN_DIR})
-endfunction(setup_main)
